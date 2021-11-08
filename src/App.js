392cb7fe54787b9f2abee5132330e96170fd5b6c
Jisha Annie Varghese <Jisha.Annie.Varghese@ibm.com>
2021-11-08 23:41:36 +0530

    my first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import CardList from "./componets/card-list/card-list.component";
+import {SearchBox} from "./componets/search-box/search-box.component";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      monsters: [],
+      searchItem: "",
+    };
+  }
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => res.json())
+      .then((d) => this.setState({ monsters: d }));
+  }
+   handleChange=(e)=>{
+  
+          this.setState({ searchItem: e.target.value }, 
+             () => {
+             console.log(this.state);
+           }
+         )
+  }
+  render() {
+    const { monsters, searchItem } = this.state;
+
+    const filteredMonster = monsters.filter((monster) => {
+     return  monster.name.toLowerCase().includes(searchItem.toLowerCase());
+    });
+    console.log("filteredMonster",filteredMonster);
+    return (
+      <div className="App">
+        <h1>Monster Robots</h1>
+       <SearchBox placeholder="search monster" handleChange={this.handleChange}/>
+        <CardList monsters={filteredMonster}/>
+      </div>
+    );
+  }
 }
-
 export default App;
---
+++ src/componets/search-box/search-box.component.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./search-box.styles.css";
+
+export const SearchBox=({placeholder,handleChange})=>{
+    return(
+        <input
+        className='search'
+        type="search"
+        placeholder={placeholder}
+        onChange={handleChange}
+            //(e) =>
+          //this.setState({ searchItem: e.target.value }, 
+          //   () => {
+          //   console.log(this.state);
+          // }
+         //)
+       // }
+      />
+    )
+}
